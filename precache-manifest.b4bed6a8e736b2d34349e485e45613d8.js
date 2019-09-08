5ea323030c15196fb3de9b261dcbe4650f1acc39
Azri Zainal <azri.zainal@lionandlion.com>
2019-09-08 18:43:01 +0800

    Updates

---
+++ precache-manifest.b4bed6a8e736b2d34349e485e45613d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5209a99dc08cd58ecbe56452c5e2cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "085c8dbc8c5bd189af89",
+    "url": "/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "085c8dbc8c5bd189af89",
+    "url": "/static/js/main.5771a0fa.chunk.js"
+  },
+  {
+    "revision": "765801b7eb73936e9c4e",
+    "url": "/static/js/runtime~main.11910b7e.js"
+  }
+]);
